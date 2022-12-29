0270ce18ea95723234bef2ee0c42ecf29d398995
abi1402 <119950900+abi1402@users.noreply.github.com>
2022-12-29 22:57:37 +0530

    Update 2.java

--- 29dec/2.java
+++ 29dec/2.java
@@ -1,40 +1,39 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
 
-public static void main(String[] args) {
-String line;
-Scanner sc = new Scanner(System.in);
-System.out.print(“\nEnter the string : “);
-line = sc.nextLine();
-int vowels = 0, consonants = 0, digits = 0, spaces = 0, symbols = 0;
+class count{
 
-line = line.toLowerCase();
-for(int i = 0; i < line.length(); ++i)
-{
-char ch = line.charAt(i);
-if(ch == ‘a’ || ch == ‘e’ || ch == ‘i’
-|| ch == ‘o’ || ch == ‘u’) {
-++vowels;
-}
-else if((ch >= ‘a’&& ch <= ‘z’)) {
-++consonants;
-}
-else if( ch >= ‘0’ && ch <= ‘9’)
-{
-++digits;
-}
-else if (ch ==’ ‘)
-{
-++spaces;
-}
-else
-++symbols;
-}
+  public static void main(String args[]) {
+    String x;
+    Scanner Scan=new Scanner(System.in);
+    System.out.println("Enter a statement:");
+    x=Scan.nextLine();
 
-System.out.println(“Vowels: ” + vowels);
-System.out.println(“Consonants: ” + consonants);
-System.out.println(“Digits: ” + digits);
-System.out.println(“White spaces: ” + spaces);
-System.out.println(“Symbols : ” + symbols);
-}
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    x = x.toLowerCase();
+    for (int i = 0; i < x.length(); ++i) {
+      char ch = x.charAt(i);
+
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowels;
+      }
+
+      else if ((ch >= 'a' && ch <= 'z')) {
+        ++consonants;
+      }
+      
+      else if (ch >= '0' && ch <= '9') {
+        ++digits;
+      }
+      
+      else if (ch == ' ') {
+        ++spaces;
+      }
+    }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+    System.out.println("Digits: " + digits);
+    System.out.println("White spaces: " + spaces);
+  }
 }
